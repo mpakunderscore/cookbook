3b9f1b5d34d318d847c3155777495b448743f19e
mpakunderscore <mpakunderscore@gmail.com>
2022-01-02 16:20:12 -0500

    Fix

--- src/ui/Pages.js
+++ src/ui/Pages.js
@@ -139,7 +139,7 @@ export default function Pages(props) {
 
         loadActivePageIndex(i)
 
-        window.scroll({left: 0, top: 86 * j})
+        window.scroll({left: 0, top: 86 * j + 11, behavior: 'smooth'})
     }
 
     let renderPages = () => {
