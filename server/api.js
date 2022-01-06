e5232049bfc01e3d4ad581e392238980485d2155
mpakunderscore <mpakunderscore@gmail.com>
2022-01-06 12:58:09 -0500

    Fix

--- server/api.js
+++ server/api.js
@@ -62,7 +62,7 @@ let loadCoreImages = async () => {
                 let queryString = recipes[j].name.split(' ').length > 1 ?
                     recipes[j].name
                     :
-                    food[i].name.replace('vegetables', 'veg') + ' ' + recipes[j].name
+                    recipes[j].name + '+' + food[i].name.replace('vegetables', 'veg')// + '+' + 'beautiful'
 
                 images[food[i].name][recipes[j].name] = await loadImages(queryString)
             }
